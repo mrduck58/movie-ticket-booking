6a961093ceafe2ee1dd9e84bbdb9babbd7e391b0
mrduck58 <huanvit0508@gmail.com>
2026-03-03 23:05:01 +0700

    #5 - Huan add app theme

---
+++ lib/core/constants/app_constant.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const appName = 'VPHAN Booking';
+  static const currencySymbol = 'VND';
+  static const locale = 'vi_VN';
+}
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -3,14 +3,44 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Brand
   static const Color primary = Color(0xFFFF5B5B);
-  static const Color bg = Colors.white;
+  static const Color onPrimary = Colors.white;
+
+  //background / surface
+  static const Color background= Colors.white;
+  //static const Color onBackground = Colors.white;
+  static const Color surface = Color.fromRGBO(222, 222, 209, 1);
+  //static const Color onSurface = Colors.white;
+
+  // default
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  //border
+  static const Color border = Color(0x1F000000); 
+  static const Color divider = Color(0xFF6B6B6B); 
 
   // Neutrals
   static const Color textPrimary = Color(0xFF111111);
   static const Color textSecondary = Color(0xFF6B6B6B);
-  static const Color border = Color(0x1F000000); 
-  static const Color placeholder = Color(0xFFE9E9E9);
+  static const Color textTertiary = Color.fromRGBO(222, 222, 209, 1);
+  //static const Color textDisabled = Colors.white;
 
-  // Status (tuỳ dùng)
+  // status
+  static const Color success = Color.fromRGBO(89, 172, 119, 1);
   static const Color error = Color(0xFFE53935);
+  static const Color warning = Color.fromRGBO(255, 195, 0, 1);
+
+  // bottom navigator
+  static const Color bottomNavBg = Colors.white;
+  static const Color bottomNavActive = Color(0xFFFF5B5B);
+  static const Color bottomNavInactive = Color.fromRGBO(125, 125, 125, 1);
+
+  // rating 
+  static const Color ratingStar = Color.fromRGBO(255, 212, 0, 1);
+
+  // seat 
+  static const Color seatAvailable = Colors.white;
+  static const Color seatSelected = Color(0xFFFF5B5B);
+  static const Color seatSold = Color.fromARGB(197, 199, 188, 1);
+
 }
