1c2a50090c699f1ec248e780812fe1567b9b0bbc
Phong <phongdvhe181343@fpt.edu.vn>
2026-03-09 13:21:15 +0700

    #15 - bottom navigation bar

--- lib/features/account/test/account_main.dart
+++ lib/features/account/test/account_main.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../presentation/pages/account_screen.dart';
+import '../../../../core/widgets/layouts/navigation_bar.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: AccountScreen(),
-      ),
+      child: TestApp(),
     ),
   );
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainLayout(
+        child: AccountScreen(),
+      ),
+    );
+  }
 }
